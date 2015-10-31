cb7b4e51e57eb78f0f3393956fd9ac9e8f86e5ce
Saad Hashim <saad_alhakim@hotmail.com>
2015-10-31 14:59:07 +0100

    Gjorde det enklare, nu behövs inte maven

--- src/main/java/se/jsta/banken/JSTABanken.java
+++ src/main/java/se/jsta/banken/JSTABanken.java
@@ -1,13 +1,19 @@
 package se.jsta.banken;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
 public interface JSTABanken {
     String sayHi(String text);
-    boolean createCustomer(String name);
-    boolean insertMoney(String name, float amount);
-    boolean withdrawMoney(String name, float amount);
-    float getBalance(String name);
+    @WebMethod(operationName="createCustomer")
+    boolean createCustomer(@WebParam(name="name") String name);
+    @WebMethod(operationName="insertMoney")
+    boolean insertMoney(@WebParam(name="name") String name, @WebParam(name="amount") float amount);
+    @WebMethod(operationName="withdrawMoney")
+    boolean withdrawMoney(@WebParam(name="name") String name,@WebParam(name="amount") float amount);
+    @WebMethod(operationName="getBalance")
+    float getBalance(@WebParam(name="name") String name);
 }
 
